bcab792c8a092596ed7ec61f57ea0da9ff108b0d
stang33 <35314130+stang33@users.noreply.github.com>
2019-06-17 22:42:46 -0400

    Update construct_and_compute_traj.m
    add the trial for the unbounded Lennard jones example.

--- SOD_Utils/construct_and_compute_traj.m
+++ SOD_Utils/construct_and_compute_traj.m
@@ -2,6 +2,38 @@ function result = construct_and_compute_traj(solver_info, sys_info, syshat_info,
 % function result = construct_and_compute_traj(solver_info, sys_info, syshat_info, T_L, L)
 
 % (c) M. Zhong
+% add the special trial for unbounded lennard jones example by Sui
+
+
+if strcmpi(sys_info.name,'LennardJonesDynamics')
+for m = 1 : size(ICs, 2)
+  y_init           = ICs(:, m);%deval(dynamics,obs_info.time_vec(1))
+  dynamics         = self_organized_dynamics(y_init, sys_info, solver_info);
+  dynamicshat      = self_organized_dynamics(deval(dynamics,0.001), syshat_info, solver_info);
+  if ~dynamics.flag && ~dynamicshat.flag, break; end
+end
+c                  = floor(solver_info.time_span(2)/T_L);
+time_vec           = linspace(0.001, solver_info.time_span(2), c * L - c + 1);
+time_vec           = insert_time_vec(time_vec, T_L);
+traj               = deval(dynamics,    time_vec);
+trajhat            = deval(dynamicshat, time_vec);
+result.traj_true   = traj(1 : sys_info.d * sys_info.N,    :);
+result.traj_hat    = trajhat(1 : sys_info.d * sys_info.N, :);
+result.time_vec    = time_vec;
+% compute the trajectory error for this random pick from training data
+time_vec           = linspace(0.001, T_L, L);
+traj               = deval(dynamics,    time_vec);
+trajhat            = deval(dynamicshat, time_vec-0.001);
+result.trajErr     = traj_norm(traj, trajhat, 'Time-Maxed', sys_info);
+time_vec           = linspace(T_L, solver_info.time_span(2), L);
+traj               = deval(dynamics,    time_vec);
+trajhat            = deval(dynamicshat, time_vec-0.001);
+result.trajErrfut  = traj_norm(traj, trajhat, 'Time-Maxed', sys_info);
+result.m           = m;
+result.dynamics    = dynamics;
+result.dynamicshat = dynamicshat;
+
+else
 
 for m = 1 : size(ICs, 2)
   y_init           = ICs(:, m);
@@ -29,4 +61,5 @@ result.trajErrfut  = traj_norm(traj, trajhat, 'Time-Maxed', sys_info);
 result.m           = m;
 result.dynamics    = dynamics;
 result.dynamicshat = dynamicshat;
+end
 end
